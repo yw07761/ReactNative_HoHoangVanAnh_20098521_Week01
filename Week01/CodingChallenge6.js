c1d8878f0eea6fe2cb4dd358573864d3c6d27b3d
yw07761 <89188722+yw07761@users.noreply.github.com>
2024-08-23 08:31:27 +0700

    Update CodingChallenge6.js

--- Week01/CodingChallenge6.js
+++ Week01/CodingChallenge6.js
@@ -1,4 +1,4 @@
-// Coding Challenge #5
+// Coding Challenge #6
 // Steven is still building his tip calculator, using the same rules as before: Tip 15% of
 // the bill if the bill value is between 50 and 300, and if the value is different, the tip is
 // 20%.
